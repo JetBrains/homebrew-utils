6dfd8bf419b056f124cd6ca2cef0447c418e6128
Sergey Shkredov <shkredov@gmail.com>
2018-06-29 01:04:17 +0300

    remove comment

--- Formula/cbs.rb
+++ Formula/cbs.rb
@@ -5,14 +5,12 @@ class Cbs < Formula
   url "https://github.com/JetBrains/rd/files/2147089/cbs-1.0.2.zip"
   sha256 "0dc359124dcf25bb8f977675652992b87f0adff9eb0b301053b1752ff62b1da3"
 
-  # depends_on "cmake" => :build
-
- def install
+  def install
     prefix.install "lib"
     bin.install "bin/cbs"
- end
+  end
 
- test do
-     system "#{bin}/cbs"
- end
+  test do
+    system "#{bin}/cbs"
+  end
 end
