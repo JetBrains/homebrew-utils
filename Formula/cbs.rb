4419e3f339b5a179b3acf95af69cad96a349b2c0
Sergey Shkredov <shkredov@gmail.com>
2018-06-29 03:24:31 +0300

    Update cbs.rb

--- Formula/cbs.rb
+++ Formula/cbs.rb
@@ -2,8 +2,8 @@ class Cbs < Formula
   desc "Circlet Build System"
   homepage "https://jetbrains.team"
 
-  url "https://github.com/JetBrains/rd/files/2147089/cbs-1.0.2.zip"
-  sha256 "0dc359124dcf25bb8f977675652992b87f0adff9eb0b301053b1752ff62b1da3"
+  url "https://dl.bintray.com/serjic/cbs/cbs-1.0.3.zip"
+  sha256 "8c3fc963dced92ed4983df523b10aba7188e43115765839a86d46ec1f2d8ded9"
 
   def install
     prefix.install "lib"
