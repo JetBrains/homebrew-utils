a50c54a2c285a31d99dd9c6755cc05b1d15a45a0
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2019-04-09 13:23:51 +0700

    Bump bunches version

--- Formula/bunches.rb
+++ Formula/bunches.rb
@@ -1,12 +1,12 @@
 class Bunches < Formula
   desc "Set of utils for supporting patchsets branches"
   homepage "https://github.com/JetBrains/bunches"
-  url "https://github.com/JetBrains/bunches/releases/download/v0.9.0/bunch-0.9.0.zip"
-  sha256 "df43a2a0a1fc4ad1f1bbe077a25c0c066790c21b7d6910954787da1d826f3b19"
+  url "https://github.com/JetBrains/bunches/releases/download/v0.9.1/bunch-cli-0.9.1.zip"
+  sha256 "204c03179eff071e3eadcfa681868f3303026f301427ffcdd9d75956650c663a"
 
   def install
-   prefix.install "lib"
-   bin.install "bin/bunch"
+    prefix.install "lib"
+    bin.install "bin/bunch"
   end
 
   test do
