2f96af2e786604dbceb2ade31fc650085501ff9f
Darya Razumova <d.razumova@yandex.ru>
2019-09-03 18:37:05 +0300

    Updated bunches version to 1.0.100

--- Formula/bunches.rb
+++ Formula/bunches.rb
@@ -1,8 +1,8 @@
 class Bunches < Formula
   desc "Set of utils for supporting patchsets branches"
   homepage "https://github.com/JetBrains/bunches"
-  url "https://github.com/JetBrains/bunches/releases/download/v1.0.73/bunch-cli-1.0.73.zip"
-  sha256 "43ba811a016441734753ec4dde7cb57bc0933d28b2f2d8f8f78cf61daa5d8f77"
+  url "https://github.com/JetBrains/bunches/releases/download/v1.0.100/bunch-cli-1.0.100.zip"
+  sha256 "6231528a23c1e82112b8399648ed89dacd518edbec79ec5bc8a690da2ca41b73"
 
   def install
     prefix.install "lib"
