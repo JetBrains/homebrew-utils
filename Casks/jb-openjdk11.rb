6e0cc2d8ed95cf055082ab303e0cae12c93a2859
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2019-09-25 11:55:01 +0200

    Update JBR SDK to 11.0.4+10-b304.69

--- Casks/jb-openjdk11.rb
+++ Casks/jb-openjdk11.rb
@@ -1,12 +1,13 @@
 cask 'jb-openjdk11' do
-  version '11.0.2,164'
-  sha256 '5de2b44d0222ee22831ba6aff8f6e02a192b21f2720af60189b7e30085eeacfc'
+  version '11.0.4,304.69'
+  sha256 '419ca38acca99582eccfca71c88f262703c7207eea19358b532916684e8bf41d'
 
-  url "https://bintray.com/jetbrains/intellij-jdk/download_file?file_path=jbr-#{version.before_comma.dots_to_underscores}-osx-x64-b#{version.after_comma}.tar.gz"
+  url "https://bintray.com/jetbrains/intellij-jbr/download_file?file_path=jbrsdk-#{version.before_comma.dots_to_underscores}-osx-x64-b#{version.after_comma}.tar.gz"
+
   name 'JetBrains OpenJDK Java Development Kit'
   homepage 'https://github.com/JetBrains/JetBrainsRuntime'
 
-  artifact "jdk", target: "/Library/Java/JavaVirtualMachines/jb-openjdk-#{version.before_comma}.jdk"
+  artifact "jbrsdk", target: "/Library/Java/JavaVirtualMachines/jb-openjdk-#{version.before_comma}.jdk"
 
   uninstall rmdir: '/Library/Java/JavaVirtualMachines'
 end
