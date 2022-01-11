dc2d6de3009ac2d16565e8003acd93ad230c44d1
jreleaserbot <jreleaser@kordamp.org>
2022-01-11 12:06:47 +0300

    ki v0.4.0

--- Formula/ki.rb
+++ Formula/ki.rb
@@ -1,24 +1,21 @@
+# Generated with JReleaser 0.10.0 at 2022-01-11T12:06:29.074575+03:00
 class Ki < Formula
-  desc "Extendable Kotlin interactive shell with code completion, and other features"
+  desc "ki shell"
   homepage "https://github.com/Kotlin/kotlin-interactive-shell"
-  url "https://github.com/Kotlin/kotlin-interactive-shell/releases/download/v0.3.3/ki-archive.zip"
-  version "0.3.3"
-  sha256 "7d4c4356390e69387fd0eae8c4607b1f0111aac236df880c081bf40f903119e7"
-  license "Apache-2.0"
-
-  bottle :unneeded
+  url "https://github.com/Kotlin/kotlin-interactive-shell/releases/download/v0.4.0/ki-archive.zip"
+  version "0.4.0"
+  sha256 "290e02f9675a7ea5bbe8c5ca0d1041ebf2b89da20ea18bfe3e10d3661784603a"
+  license "Apache-2"
 
   depends_on "openjdk@8"
 
   def install
-    libexec.install "bin", "lib"
-    rm Dir["#{libexec}/bin/*.bat"]
-    bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/ki"
   end
 
   test do
-    output = shell_output("#{bin}/ki.sh <<< '' 2>&1 | grep ki-shell")
-    assert_match "ki-shell 0.3/1.4.32", output
+    output = shell_output("#{bin}/ki --version")
+    assert_match "0.4.0", output
   end
 end
