4640a66b1d65e66495728f2c1a94557c1cc14ae2
itomise <itomise.mizumi@gmail.com>
2025-12-14 19:59:54 +0900

    kotlin-lsp: bump to v261.13587.0
    - Add architecture-specific builds (mac/linux x64/aarch64)
    - Remove openjdk dependency (JRE now bundled)
    
    closes https://github.com/JetBrains/homebrew-utils/pull/12

--- Formula/kotlin-lsp.rb
+++ Formula/kotlin-lsp.rb
@@ -1,9 +1,7 @@
 class KotlinLsp < Formula
   desc "Official Language Server Protocol for the Kotlin language"
   homepage "https://github.com/Kotlin/kotlin-lsp"
-  version "0.253.10629"
-  url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-#{version}.zip"
-  sha256 "afa071874081f644ea6a85b4a864576effb6afa8ee30ee7511199cdccdac379c"
+  version "261.13587.0"
   license "Apache-2.0"
 
   livecheck do
@@ -11,13 +9,27 @@ class KotlinLsp < Formula
     regex(/^(?:#)+\s*v(.*)$/i)
   end
 
-  depends_on "openjdk"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-lsp-#{version}-mac-x64.zip"
+      sha256 "a3972f27229eba2c226060e54baea1c958c82c326dfc971bf53f72a74d0564a3"
 
-  def install
-    chmod "+x", "kotlin-lsp.sh"
-    libexec.install Dir["*"]
+      def install
+        chmod "+x", "kotlin-lsp.sh"
+        libexec.install Dir["*"]
+        bin.install_symlink "#{libexec}/kotlin-lsp.sh" => "kotlin-lsp"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-lsp-#{version}-mac-aarch64.zip"
+      sha256 "d4ea28b22b29cf906fe16d23698a8468f11646a6a66dcb15584f306aaefbee6c"
 
-    bin.install_symlink "#{libexec}/kotlin-lsp.sh" => "kotlin-lsp"
+      def install
+        chmod "+x", "kotlin-lsp.sh"
+        libexec.install Dir["*"]
+        bin.install_symlink "#{libexec}/kotlin-lsp.sh" => "kotlin-lsp"
+      end
+    end
   end
 
   test do
