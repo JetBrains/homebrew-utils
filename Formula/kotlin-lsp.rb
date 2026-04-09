40502b52f4095673ef36ca5ab4880323cad9515a
iTiPo <iTiPo@users.noreply.github.com>
2026-04-09 19:25:22 +0300

    Update Kotlin LSP version to 262.2310.0

--- Formula/kotlin-lsp.rb
+++ Formula/kotlin-lsp.rb
@@ -1,7 +1,7 @@
 class KotlinLsp < Formula
   desc "Official Language Server Protocol for the Kotlin language"
   homepage "https://github.com/Kotlin/kotlin-lsp"
-  version "261.13587.0"
+  version "262.2310.0"
   license "Apache-2.0"
 
   livecheck do
@@ -15,7 +15,7 @@ class KotlinLsp < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-lsp-#{version}-mac-x64.zip"
-      sha256 "a3972f27229eba2c226060e54baea1c958c82c326dfc971bf53f72a74d0564a3"
+      sha256 "a4ccf591664cfef6a12f21a690d23bad26b92de62ed34674491b915f25f95bf5"
 
       def install
         chmod "+x", "kotlin-lsp.sh"
@@ -25,7 +25,7 @@ class KotlinLsp < Formula
     end
     if Hardware::CPU.arm?
       url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-lsp-#{version}-mac-aarch64.zip"
-      sha256 "d4ea28b22b29cf906fe16d23698a8468f11646a6a66dcb15584f306aaefbee6c"
+      sha256 "11560eb4ecd766204363848cc5ee84b51c0fd03fbfd4bbedaba0f00af74309c7"
 
       def install
         chmod "+x", "kotlin-lsp.sh"
