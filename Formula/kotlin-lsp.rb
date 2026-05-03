188d984fe904f674fe6565d59b5b7deb2e5a5f44
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2026-05-03 11:43:10 +0200

    Update kotlin-lsp to 262.4739 (#15)
    Also, follow 'brew style' recommendations w.r.t. livecheck

--- Formula/kotlin-lsp.rb
+++ Formula/kotlin-lsp.rb
@@ -1,12 +1,12 @@
 class KotlinLsp < Formula
   desc "Official Language Server Protocol for the Kotlin language"
   homepage "https://github.com/Kotlin/kotlin-lsp"
-  version "262.2310.0"
+  version "262.4739.0"
   license "Apache-2.0"
 
   livecheck do
-    url "https://raw.githubusercontent.com/Kotlin/kotlin-lsp/refs/heads/main/RELEASES.md"
-    regex(/^(?:#)+\s*v(.*)$/i)
+    url :homepage
+    strategy :github_latest
   end
 
   # We don't want to support brew packages on Linux (On Linux, people should be using their native package managers)
@@ -14,29 +14,27 @@ class KotlinLsp < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-lsp-#{version}-mac-x64.zip"
-      sha256 "a4ccf591664cfef6a12f21a690d23bad26b92de62ed34674491b915f25f95bf5"
-
-      def install
-        chmod "+x", "kotlin-lsp.sh"
-        libexec.install Dir["*"]
-        bin.install_symlink "#{libexec}/kotlin-lsp.sh" => "kotlin-lsp"
-      end
+      url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}.sit",
+          using: :nounzip
+      sha256 "6f06efe7a10f94b9c8a028c4efeb6c7e1769f47a01edfb74450acf30ab5665e4"
     end
     if Hardware::CPU.arm?
-      url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-lsp-#{version}-mac-aarch64.zip"
-      sha256 "11560eb4ecd766204363848cc5ee84b51c0fd03fbfd4bbedaba0f00af74309c7"
-
-      def install
-        chmod "+x", "kotlin-lsp.sh"
-        libexec.install Dir["*"]
-        bin.install_symlink "#{libexec}/kotlin-lsp.sh" => "kotlin-lsp"
-      end
+      url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}-aarch64.sit",
+          using: :nounzip
+      sha256 "1b745743ce22ad92681a1bc3b1046803e942a6e1f36e04fb85ae9a40334a2f1e"
     end
   end
 
+  def install
+    archive_name = "kotlin-server-#{version}"
+    archive_name += "-aarch64" if Hardware::CPU.arm?
+
+    system "unzip", "-q", "#{archive_name}.sit"
+    libexec.install Dir["kotlin-server-#{version}/*"]
+    bin.install_symlink "#{libexec}/bin/intellij-server" => "kotlin-lsp"
+  end
+
   test do
-    # kotlin-lsp doesn't have --version flag
-    assert_equal "Usage: kotlin-lsp [OPTIONS]", shell_output("#{bin}/kotlin-lsp -h").split("\n").first
+    assert_match "LS-#{version}", shell_output("#{bin}/kotlin-lsp --version")
   end
 end
